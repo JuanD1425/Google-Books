ae0bc6930cf0d6bd7b901ea6ce4b2245677296e9
Juan Diego Gutierrez <JuanD1425@me.com>
2019-12-07 06:28:36 -0500

    build

---
+++ client/build/precache-manifest.86a77ad1aa1cf3890a7e263bb7d3175c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ffb681832105db02e77cbf7ea5dd2cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "066cd2b46c64ac1f9a48",
+    "url": "/static/css/main.0a49dd09.chunk.css"
+  },
+  {
+    "revision": "2c47dbd1cad8e4e7c58f",
+    "url": "/static/js/2.9ff7b941.chunk.js"
+  },
+  {
+    "revision": "066cd2b46c64ac1f9a48",
+    "url": "/static/js/main.94383f5c.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  }
+]);
